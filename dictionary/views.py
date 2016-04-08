9ef1baf49550ea44eeb61071d3c7fc81fa668f08
Matumaros <mat@boar.bar>
2016-04-08 21:37:37 +0200

    The GO button works now

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,18 +1,24 @@
 
 
 from django.views import generic
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Word, Language
 
 
 def dict_view(request, origin='ENG', target='BAR', word=''):
-    words = Word.objects.filter(word__regex=word, language__name=origin)
-    kwargs = {
-        'words': words,
-        'origin_word': word,
-        'languages': Language.objects,
-        'origin': origin,
-        'target': target,
-    }
-    return render(request, 'dictionary/main.html', kwargs)
+    if request.method == 'POST':
+        origin = request.POST['origin']
+        target = request.POST['target']
+        word = request.POST['word']
+        return redirect('/dict/{}{}/{}'.format(origin, target, word))
+    else:
+        words = Word.objects.filter(word__regex=word, language__name=origin)
+        kwargs = {
+            'words': words,
+            'origin_word': word,
+            'languages': Language.objects,
+            'origin': origin,
+            'target': target,
+        }
+        return render(request, 'dictionary/main.html', kwargs)
