044669847b0e641a33b2732830403555030bc4c9
Matthias Schreiber <mat@boar.bar>
2016-07-09 17:36:04 +0200

    main model for collections

--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,11 @@
+
+
 from django.db import models
 
-# Create your models here.
+
+class CollectionItem(models.Model):
+    author = models.CharField(max_length=100)
+    reporter = models.CharField(max_length=100)
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    creation_date = models.DateField(auto_now_add=True)
