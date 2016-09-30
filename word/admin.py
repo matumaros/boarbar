7980ebbfa78ba69c3c3dc7e0677544029904222f
Matthias Schreiber <mat@boar.bar>
2016-09-30 17:53:26 +0200

    added WorldHistory to the admin

--- word/admin.py
+++ word/admin.py
@@ -3,8 +3,9 @@
 from django.contrib import admin
 
 
-from .models import Tag, Word
+from .models import Tag, Word, WordHistory
 
 
 admin.site.register(Tag)
 admin.site.register(Word)
+admin.site.register(WordHistory)
