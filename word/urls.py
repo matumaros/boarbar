cd92ba64602f090ba7c23541cda87a35f1bb4c4a
Matthias Schreiber <mat@boar.bar>
2016-11-20 15:01:25 +0100

    #21 created page to create suggestions

--- word/urls.py
+++ word/urls.py
@@ -8,4 +8,5 @@ from . import views
 urlpatterns = [
     # url(r'^$', views.table_view, name='table_view'),
     url(r'^(?P<word_id>[0-9]*)$', views.word_view, name='word_view'),
+    url(r'^suggest/?$', views.suggest_view, name='suggest_view'),
 ]
--- word/views.py
+++ word/views.py
@@ -1,14 +1,36 @@
 
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import BavarianWord
+from .models import BavarianWord, Description
 from language.models import Language
 
 
 def word_view(request, word_id):
     kwargs = {
-        'languages': Language.objects,
         'word': BavarianWord.objects.get(pk=word_id),
     }
     return render(request, 'word/display.html', kwargs)
+
+
+def suggest_view(request):
+    if request.method == 'POST':
+        word = request.POST['word']
+        description_short = request.POST['desc_short']
+        description_long = request.POST['desc_long']
+
+        language = Language.objects.get(name='BAR')
+        desc = Description.objects.create(
+            short=description_short,
+            extended=description_long,
+            language=language,
+        )
+        word = BavarianWord.objects.create(
+            word=word,
+            status='SUG',
+            version='boarV1',
+        )
+        word.desc.add(desc)
+        return redirect('/word/{}'.format(word.id))
+    else:
+        return render(request, 'word/suggest.html')
