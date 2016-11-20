c1aacc03d1fa4989edac0f713627c261574e50ab
Denys Tostohan <titan074@gmail.com>
2016-11-20 18:31:52 +0200

    Don't add migration folder and remove os.environ from settings

--- boar_bar/settings/__init__.py
+++ boar_bar/settings/__init__.py
@@ -21,8 +21,7 @@ BASE_DIR = dirname(dirname(dirname(os.path.abspath(__file__))))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# Keep secret key at the file or environment variable
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
