5bf2babf4bedbce13cbac91817e59a9f1919974f
Denys Tostohan <titan074@gmail.com>
2016-11-20 18:04:55 +0200

    Add and update requirements. Set secret key based on environ class

--- boar_bar/settings/__init__.py
+++ boar_bar/settings/__init__.py
@@ -21,7 +21,8 @@ BASE_DIR = dirname(dirname(dirname(os.path.abspath(__file__))))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+# Keep secret key at the file or environment variable
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
