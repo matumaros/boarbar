629da48e7f015df304019c3f88745922fff1a897
Denys Tostohan <titan074@gmail.com>
2016-11-22 21:14:15 +0200

    Fix custom login. Fix pep8 for boar_bar.urls

--- share/views.py
+++ share/views.py
@@ -1,25 +1,7 @@
 
 
-from django.contrib.auth import authenticate, login as auth, logout as out
+from django.contrib.auth import logout as out
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-
-
-def login(request):
-    if request.method == 'POST':
-        if 'login' in request.POST:
-            username = request.POST['username']
-            password = request.POST['password']
-            user = authenticate(username=username, password=password)
-            if user:
-                if user.is_active:
-                    auth(request, user)
-                    return redirect(request.POST['previous'])
-                else:
-                    return HttpResponse('Your account is disabled.')
-            else:
-                return HttpResponse('Invalid login details supplied.')
-    return redirect('/admin/')
 
 
 def logout(request):
