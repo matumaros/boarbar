00781afcf28c618e82677554bfdb9b7cda05f5b4
Matthias Schreiber <mat@boar.bar>
2016-11-25 18:18:16 +0100

    closed #39

--- proposal/models.py
+++ proposal/models.py
@@ -1,3 +1,21 @@
+
+
 from django.db import models
 
-# Create your models here.
+from simple_history.models import HistoricalRecords
+
+
+class ProposalTopic(models.Model):
+    STATUS = (
+        ('open', 'open'),
+        ('closed', 'closed'),  # not possible to add proposals
+    )
+
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    creation_date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=STATUS)
+    history = HistoricalRecords()
+
+    def __str__(self):
+        return self.title
