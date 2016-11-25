2b9eaa1041ff84f74e6253cabceef512876462e4
Matthias Schreiber <mat@boar.bar>
2016-11-25 18:28:52 +0100

    closed #38

--- proposal/models.py
+++ proposal/models.py
@@ -19,3 +19,25 @@ class ProposalTopic(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Proposal(models.Model):
+    STATUS = (
+        ('suggested', 'suggested'),  # editable by the creator,
+                                     # possibility to add comments
+        ('proposed', 'proposed'),  # not editable anymore, people can vote
+        ('accepted', 'accepted'),
+        ('declined', 'declined'),
+    )
+
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    creation_date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=STATUS)
+    topic = models.ForeignKey(ProposalTopic)
+    history = HistoricalRecords()
+
+    def __str__(self):
+        return self.title
