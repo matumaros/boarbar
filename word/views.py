cbc36ee098854fac2de4f78e3992710a6fb02d77
matumaros <mat@boar.bar>
2017-08-12 18:15:31 +0200

    fixed word suggestion view

--- word/views.py
+++ word/views.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, TemplateView, ListView
 
-from .models import Word, Description
+from .models import Word, Description, WordVersion, WordLocation
 from language.models import Language
 
 
@@ -28,23 +28,43 @@ class SuggestView(TemplateView):
     template_name = 'word/suggest.html'
     http_method_names = ['get', 'post']
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update({
+            'versions': WordVersion.objects.all(),
+        })
+        return context
+
     def post(self, request, *args, **kwargs):
         word = request.POST.get('word')
+        ipa = request.POST.get('ipa')
+        version = request.POST.get('version')
+        location = request.POST.get('location')
         description_short = request.POST.get('desc_short')
         description_long = request.POST.get('desc_long')
 
-        language = Language.objects.get(name='BAR')
+        version = WordVersion.objects.get(pk=version)
+
         desc = Description.objects.create(
             short=description_short,
             extended=description_long,
-            language=language,
+            language=version.language,
         )
         word = Word.objects.create(
             word=word,
+            ipa=ipa,
             status='SUG',
-            version='1',
+            version=version,
+            submitter=request.user.profile,
         )
         word.desc.add(desc)
+        if location:
+            location = WordLocation.objects.create(
+                word=word,
+                place=location,
+                submitter=request.user.profile,
+            )
 
         url = reverse_lazy('word:word_view', kwargs={'pk': word.id})
         return HttpResponseRedirect(url)
