a35e5b466711b5eb8fa0358a802965b72e92d450
matumaros <mat@boar.bar>
2017-08-22 21:53:26 +0200

    changed url patterns for word

--- word/urls.py
+++ word/urls.py
@@ -7,8 +7,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.WordListView.as_view(), name='word_list_view'),
-    url(r'^(?P<pk>\d+)/bare/$', views.BareWordView.as_view(), name='bare_word_view'),
-    url(r'^(?P<pk>\d+)/edit/$', views.EditView.as_view(), name='edit_view'),
-    url(r'^(?P<pk>\d+)$', views.WordView.as_view(), name='word_view'),
+    url(r'^bare/(?P<pk>\d+)/$', views.BareWordView.as_view(), name='bare_word_view'),
+    url(r'^edit/(?P<pk>\d+)/$', views.EditView.as_view(), name='edit_view'),
+    url(r'^view/(?P<pk>\d+)$', views.WordView.as_view(), name='word_view'),
     url(r'^suggest/$', views.SuggestView.as_view(), name='suggest_view'),
 ]
