9ff539f4d9cd59711dc7fe8a394c956a8ff9db33
matumaros <mat@boar.bar>
2017-08-23 16:46:34 +0200

    user can now only edit words if they are authenticated and have a certain reputation

--- word/urls.py
+++ word/urls.py
@@ -1,6 +1,7 @@
 
 
 from django.conf.urls import url
+from django.contrib.auth.decorators import user_passes_test
 
 from . import views
 
@@ -8,7 +9,14 @@ from . import views
 urlpatterns = [
     url(r'^$', views.WordListView.as_view(), name='word_list_view'),
     url(r'^bare/(?P<pk>\d+)/$', views.BareWordView.as_view(), name='bare_word_view'),
-    url(r'^edit/(?P<pk>\d+)/$', views.EditView.as_view(), name='edit_view'),
+    url(r'^edit/(?P<pk>\d+)/$',
+        user_passes_test(
+            lambda u: u.is_authenticated and u.profile.reputation >= 100,
+            login_url=''
+        )(
+            views.EditView.as_view()
+        ),
+        name='edit_view'),
     url(r'^view/(?P<pk>\d+)$', views.WordView.as_view(), name='word_view'),
     url(r'^suggest/$', views.SuggestView.as_view(), name='suggest_view'),
 ]
