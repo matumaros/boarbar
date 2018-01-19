6af67717a9be01188de91f168f7d633592726d18
carol <hedonazur@hotmail.com>
2018-01-18 23:47:11 -0500

    turn debug to False

--- boar_bar/settings/__init__.py
+++ boar_bar/settings/__init__.py
@@ -28,7 +28,7 @@ BASE_DIR = dirname(dirname(dirname(os.path.abspath(__file__))))
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'www.boar.bar',
--- boar_bar/urls.py
+++ boar_bar/urls.py
@@ -19,7 +19,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import RedirectView
-from django.template import RequestContext
 
 from share.views import NotExisting, Logout
 
