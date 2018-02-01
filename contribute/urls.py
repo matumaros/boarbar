1f7445644be907456825bdc65a0cdd5bc452f9e5
carol <hedonazur@hotmail.com>
2018-02-01 09:47:12 -0500

    spaces

--- contribute/urls.py
+++ contribute/urls.py
@@ -11,10 +11,4 @@ urlpatterns = [
         views.ContribView.as_view(),
         name='contrib_view'
     ),
-    # dict with origin language, target language and word
-    # url(
-    #     r'^(?P<sourcelang>[A-Z]{3})/(?P<word>.*)$',
-    #     views.DictAfterSearchView.as_view(),
-    #     name='dict_view_after_search'
-    # ),
 ]
