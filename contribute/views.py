4d0c9f1cc821c003dae0388ccd1cbe0c4c115fd0
carol <hedonazur@hotmail.com>
2018-02-10 15:31:32 -0500

    add selectize to synonyms, for moderator and no moderator

--- contribute/views.py
+++ contribute/views.py
@@ -4,18 +4,25 @@ from django.http import HttpResponse
 from user.models import Profile, UserLanguage
 from word.models import Tag, Word, Language
 
+
 class ContribView(TemplateView):
     template_name = 'contribute/main.html'
 
     def get(self, request, *args, **kwargs):
-        user_profile = Profile.objects.get(user=request.user)
-        user_language = UserLanguage.objects.get(user=user_profile)
-        if user_language.is_moderator:
-            user_moderator = True
-        else:
-            user_moderator = False
         context = self.get_context_data(**kwargs)
-        context["user_moderator"] = user_moderator
+
+        try:
+            user_profile = Profile.objects.get(user=request.user)
+        except TypeError:
+            user_profile = None
+
+        if user_profile:
+            user_language = UserLanguage.objects.get(user=user_profile)
+            if user_language.is_moderator:
+                user_moderator = True
+            else:
+                user_moderator = False
+            context["user_moderator"] = user_moderator
         return self.render_to_response(context)
 
     def get_context_data(self, **kwargs):
