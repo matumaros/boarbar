81b1096d1afde1958427eceb6ce92b4907c23570
matumaros <mat@boar.bar>
2018-02-17 14:26:03 +0100

    added default_variant field to language which represents the default localization to use

--- language/models.py
+++ language/models.py
@@ -5,6 +5,11 @@ from django.db import models
 
 class Language(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    default_variant = models.CharField(
+        max_length=5,
+        blank=True,
+        help_text="Variant of the language such as 'bv_DA' for Danube Bavarian"
+    )
 
     def __str__(self):
         return self.name
