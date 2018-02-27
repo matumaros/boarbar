4e1e0324e7a5fe3d863f8f441122b9e5ef84755e
carol <hedonazur@hotmail.com>
2018-02-27 17:54:52 -0500

    template sign up works and adds place, language and proficiency

---
+++ user/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from user.models import Language, UserLanguage
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    language = forms.ModelChoiceField(queryset=Language.objects.all())
+    place = forms.CharField(max_length=100)
+    proficiency = forms.ChoiceField(choices=UserLanguage.PROF)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+
+
+
--- user/views.py
+++ user/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import DetailView
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from user.forms import SignUpForm
 
-from .models import Profile
+from user.models import UserLanguage, Profile
 
 
 class ProfileView(DetailView):
@@ -16,14 +16,29 @@ class ProfileView(DetailView):
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+            language = form.cleaned_data.get("language")
+            place = form.cleaned_data.get("place")
+            proficiency = form.cleaned_data.get("proficiency")
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
+            django_user = authenticate(username=username, password=raw_password)
+            login(request, django_user)
+            user_profile = Profile.objects.create(
+                user=django_user,
+                place=place,
+            )
+            user_language = UserLanguage.objects.create(
+                user=user_profile,
+                language=language,
+                proficiency=proficiency,
+            )
+            return redirect('/')
     else:
-        form = UserCreationForm()
-    return render(request, 'user/signup.html', {'form': form})
+        form = SignUpForm()
+
+    return render(request, 'user/signup.html', {'form':form})
+
+
