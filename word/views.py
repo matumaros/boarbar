8846f4c41fa737c0e2f4da657dadd9ab069332c1
matumaros <mat@boar.bar>
2018-03-15 08:25:50 +0100

    Word descriptions are no longer created if both the short and long description are empty; code refactoring

--- word/views.py
+++ word/views.py
@@ -121,7 +121,7 @@ class SuggestView(TemplateView):
         return HttpResponseRedirect(url)
 
     def create_descriptions(self, request):
-        """Function to generate list of description objects for one word,
+        """Generate list of description objects for one word,
         each description object is in a different language and contains both
         short and long description strings
 
@@ -132,32 +132,22 @@ class SuggestView(TemplateView):
         :param request: original django request
         :return: list of Description objects
         """
-        descriptions = dict()
-        for key in request.POST.keys():
-            if "desc_long" in key:
-                desc_long_string = request.POST.get(key)
-                language = key.split("_")[-1]
-                if language not in descriptions:
-                    descriptions[language] = dict()
-                descriptions[language]["desc_long"] = desc_long_string
-
-            if "desc_short_" in key:
-                # desc_short_spanish
-                desc_short_string = request.POST.get(key)
-                language = key.split("_")[-1]
-                if language not in descriptions:
-                    descriptions[language] = dict()
-                descriptions[language]["desc_short"] = desc_short_string
-        desc_list = []
-        for language, language_desc in descriptions.items():
-            language_obj = Language.objects.get(name=language)
-            desc, _ = Description.objects.get_or_create(
-                language=language_obj,
-                short=language_desc["desc_short"],
-                extended=language_desc["desc_long"],
-            )
-            desc_list.append(desc)
-        return desc_list
+        descriptions = {}
+        for key, value in request.POST.items():
+            if not value:
+                continue
+            if key.startswith("desc_long") or key.startswith("desc_short"):
+                key, language = key.rsplit("_", 1)
+                try:
+                    desc = descriptions[language]
+                except KeyError:
+                    language_obj = Language.objects.get(name=language)
+                    desc = Description(language=language_obj)
+                    descriptions[language] = desc
+                setattr(desc, key, value)
+        descriptions = list(descriptions.values())
+        Description.objects.bulk_create(descriptions)
+        return descriptions
 
 
 class EditView(UpdateView):
@@ -166,53 +156,6 @@ class EditView(UpdateView):
         'word', 'ipa', 'desc', 'tags', 'audio', 'wiktionary_link', 'synonyms'
     ]
     template_name_suffix = '_update_form'
-
-    # template_name = 'word/edit.html'
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-
-    #     pk = kwargs.get('pk')
-    #     word = Word.objects.get(pk=pk)
-
-    #     context.update({
-    #         'word': word,
-    #         'versions': WordVersion.objects.all(),
-    #     })
-    #     return context
-
-    # def post(self, request, *args, **kwargs):
-    #     word = request.POST.get('word')
-    #     ipa = request.POST.get('ipa')
-    #     version = request.POST.get('version')
-    #     location = request.POST.get('location')
-    #     description_short = request.POST.get('desc_short')
-    #     description_long = request.POST.get('desc_long')
-
-    #     version = WordVersion.objects.get(pk=version)
-
-    #     desc = Description.objects.create(
-    #         short=description_short,
-    #         extended=description_long,
-    #         language=version.language,
-    #     )
-    #     word = Word.objects.create(
-    #         word=word,
-    #         ipa=ipa,
-    #         status='SUG',
-    #         version=version,
-    #         submitter=request.user.profile,
-    #     )
-    #     word.desc.add(desc)
-    #     if location:
-    #         location = WordLocation.objects.create(
-    #             word=word,
-    #             place=location,
-    #             submitter=request.user.profile,
-    #         )
-
-    #     url = reverse_lazy('word:word_view', kwargs={'pk': word.id})
-    #     return HttpResponseRedirect(url)
 
 
 class WordListView(ListView):
