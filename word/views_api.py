6f874d3d56fedaea1d96a3562d640b0827a2daa2
carol <hedonazur@hotmail.com>
2018-03-19 23:58:22 -0500

    similar words no longer pile and api only works if some word is type

--- word/views_api.py
+++ word/views_api.py
@@ -14,19 +14,22 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 def similar_words(request):
     if request.GET:
         base_word = request.GET.get("word")
-        list_of_words = list(Word.objects.all().values_list("word", flat=True))
-        # fuzzywuzzy: Get a list of matches ordered by score, the top 3
-        results = process.extract(base_word, list_of_words)[0:3]
-        list_of_similar_words = [x[0] for x in results]
-        words_and_ids = []
+        if base_word:
+            list_of_words = list(Word.objects.all().values_list("word", flat=True))
+            # fuzzywuzzy: Get a list of matches ordered by score, the top 3
+            results = process.extract(base_word, list_of_words)[0:3]
+            list_of_similar_words = [x[0] for x in results]
+            words_and_ids = []
 
-        for word in list_of_similar_words:
-            word_obj = Word.objects.filter(word=word).first()
-            word_dict = {"word": word, "id": word_obj.id}
-            words_and_ids.append(word_dict)
+            for word in list_of_similar_words:
+                word_obj = Word.objects.filter(word=word).first()
+                word_dict = {"word": word, "id": word_obj.id}
+                words_and_ids.append(word_dict)
 
-        response = {"similar_words_found": words_and_ids}
-        return JsonResponse(response)
+            response = {"similar_words_found": words_and_ids}
+            return JsonResponse(response)
+        else:
+            return JsonResponse({})
     else:
         response = {"error": "do GET request"}
         return JsonResponse(response)
