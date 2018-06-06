13b11268473a73c9ddebec8d94a52cb56b77b238
carol <hedonazur@hotmail.com>
2018-06-06 10:00:25 -0500

    added all fields

--- word/forms.py
+++ word/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from word.models import Word
+from word.models import Word, Description, Tag
 
 
 class WordForm(forms.ModelForm):
@@ -16,7 +16,18 @@ class EditForm(forms.ModelForm):
     ipa = forms.CharField(
         max_length=100, required=False, widget=forms.TextInput(attrs={'class': "form-control"})
     )
-
+    desc = forms.ModelChoiceField(queryset=Description.objects.all(),
+                                      widget=forms.SelectMultiple(attrs={'class': 'form-control'})
+    )
+    tags = forms.ModelChoiceField(queryset=Tag.objects.all(),
+                                  widget=forms.SelectMultiple(attrs={'class': 'form-control'})
+                                  )
+    wiktionary_link = forms.CharField(
+        max_length=100, required=False, widget=forms.TextInput(attrs={'class': "form-control"})
+    )
+    synonyms = forms.ModelChoiceField(queryset=Word.objects.all(),
+                                  widget=forms.SelectMultiple(attrs={'class': 'form-control'})
+    )
     class Meta:
         model = Word
         fields = ('word', 'ipa', 'desc', 'tags', 'audio', 'wiktionary_link', 'synonyms')
